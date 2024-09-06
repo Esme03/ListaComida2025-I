5adbaa59d83e1645fcbbaa99d47a9e57770289bc
HP <esmeraldabarriosgonzalez@gmail.com>
2024-09-05 23:14:50 -0600

    fuentes y temas

--- app/src/main/java/com/example/listacomida2025_i/ui/views/MenuCard.kt
+++ app/src/main/java/com/example/listacomida2025_i/ui/views/MenuCard.kt
@@ -25,10 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,7 +48,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surfaceContainerHighest
                 ) {
                 }
             }
@@ -80,7 +82,10 @@ fun MenuCard(platillo: Platillo, modifier: Modifier=Modifier){
                     modifier = Modifier
                         .fillMaxWidth(),
                     textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.headlineSmall
+                    style = MaterialTheme.typography.displayLarge,
+                    fontWeight = FontWeight.W300,
+                    color = Color.Black
+
                 )
                 Spacer(modifier = modifier.height(7.dp))
                 Text(
@@ -88,7 +93,9 @@ fun MenuCard(platillo: Platillo, modifier: Modifier=Modifier){
                     modifier = Modifier
                         .fillMaxWidth(),
                     textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Green
+
                 )
                 Spacer(modifier = modifier.height(7.dp))
                 Text(
@@ -96,7 +103,8 @@ fun MenuCard(platillo: Platillo, modifier: Modifier=Modifier){
                     modifier = Modifier
                         .fillMaxWidth(),
                     textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.bodySmall
+                    style = MaterialTheme.typography.labelLarge,
+                    color = Color.Red
                 )
             }
         }
